f41188205ce9c05c9fd0f7b249e2a58ca03506fa
gerau <cholkegn@gmail.com>
2024-10-07 17:59:42 +0300

    removed print statement

--- src/LongNumberModulo.cpp
+++ src/LongNumberModulo.cpp
@@ -5,7 +5,6 @@ LongNumberModulo::LongNumberModulo(LongNumber& mod)
     modulo = mod;
     mu = calculateMu(mod);
     digit_length = modulo.digitLength();
-
 }
 
 LongNumber LongNumberModulo::add(LongNumber& first, LongNumber& second)
@@ -21,8 +20,7 @@ LongNumber LongNumberModulo::sub(LongNumber& first, LongNumber& second)
         result = second - first;
         result = barrett(result);
         result = modulo - result;
-    }
-    else {
+    } else {
         result = first - second;
         result = barrett(result);
     }
@@ -32,14 +30,12 @@ LongNumber LongNumberModulo::sub(LongNumber& first, LongNumber& second)
 LongNumber LongNumberModulo::mult(LongNumber& first, uint32_t& second)
 {
     LongNumber result = first * second;
-
     return barrett(result);
 }
 
 LongNumber LongNumberModulo::mult(LongNumber& first, LongNumber& second)
 {
     LongNumber result = first * second;
-    std::cout << result << '\n';
     return barrett(result);
 }
 
@@ -52,7 +48,6 @@ LongNumber LongNumberModulo::square(LongNumber& number)
 LongNumber LongNumberModulo::power(LongNumber& number, LongNumber& power)
 {
     LongNumber result(1);
-
     for (int i = power.bitLength() - 1; i >= 0; i--) {
         uint32_t bit = power.number_array[i / 32] >> i % 32;
         bit = bit & 1;
@@ -60,12 +55,11 @@ LongNumber LongNumberModulo::power(LongNumber& number, LongNumber& power)
             result = mult(number, result);
         }
         if (i > 0) {
-            result = mult(result,result);
+            result = mult(result, result);
         }
     }
     return result;
 }
-
 
 LongNumber LongNumberModulo::barrett(LongNumber& number)
 {
